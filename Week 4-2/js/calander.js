32fa8d2b940845d5b16b7e3ca00ad1e69293dff5
DESKTOP-TGOT33V\Poom <pansawutk@gmail.com>
2016-12-13 13:54:17 +0700

    no message

---
+++ Week 4-2/js/calander.js
@@ -0,0 +1,15 @@
+var calender = {
+				today: new Date(),
+				holiday: [],
+				myEvent: function() {
+
+				},
+				leave: function() {
+
+				}
+};
+
+calender.today
+calander.holiday
+calander.myEvent()
+calander.leave()
